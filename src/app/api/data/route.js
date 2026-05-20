e874c575618644e92ed6eb610d5c56d7c40553c7
Guido Zimmermann <guidozimmermann@MacBook-Pro.local>
2026-05-20 15:17:28 -0300

    Fix data caching issue by forcing dynamic route

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,6 +1,8 @@
 import { db } from '@/app/lib/db';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const clients = await db.getClients();
   let campaigns = [];
